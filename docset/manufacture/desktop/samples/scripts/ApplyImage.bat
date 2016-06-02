7c7708ad51e31e445f979d23cfa03f97771e9a4c
Ken Pacquer <kpacquer@hotmail.com>
2016-06-02 14:05:46 -0700

    Cleaned up ApplyImage script

--- docset/manufacture/desktop/samples/scripts/ApplyImage.bat
+++ docset/manufacture/desktop/samples/scripts/ApplyImage.bat
@@ -23,10 +23,9 @@ for /f "tokens=2* delims=	 " %%A in ('reg query HKLM\System\CurrentControlSet\Co
 @if %Firmware%==0x1 echo ...using BIOS (MBR) format and partitions.
 @if %Firmware%==0x2 echo ...using UEFI (GPT) format and partitions. 
 @echo CAUTION: All the data on the disk will be DELETED.
-@SET /P M=Erase all data and continue? (Y or N):
-@if %M%.==Y. set READYSET=GO
-@if %M%.==y. set READYSET=GO
-@if not %READYSET%.==GO. goto END
+@SET /P READY=Erase all data and continue? (Y or N):
+@if %READY%.==y. set READY=Y
+@if not %READY%.==Y. goto END
 if %Firmware%==0x1 diskpart /s %~dp0CreatePartitions-BIOS.txt
 if %Firmware%==0x2 diskpart /s %~dp0CreatePartitions-UEFI.txt
 @echo *********************************************************************
@@ -34,12 +33,11 @@ if %Firmware%==0x2 diskpart /s %~dp0CreatePartitions-UEFI.txt
 call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 @echo *********************************************************************
 @echo  == Apply the image to the Windows partition ==
-@SET /P M=Deploy as Compact OS? (Y or N):
-@if %M%.==Y. set COMPACTOS=Yes
-@if %M%.==y. set COMPACTOS=Yes
-@if %COMPACTOS%.==Yes. echo Deploying as Compact OS.
-if %COMPACTOS%.==Yes. dism /Apply-Image /ImageFile:%1 /Index:1 /ApplyDir:W:\ /Compact
-if not %COMPACTOS%.==Yes. dism /Apply-Image /ImageFile:%1 /Index:1 /ApplyDir:W:\
+@SET /P COMPACTOS=Deploy as Compact OS? (Y or N):
+@if %COMPACTOS%.==y. set COMPACTOS=Y
+@if %COMPACTOS%.==Y. echo Deploying as Compact OS.
+if %COMPACTOS%.==Y. dism /Apply-Image /ImageFile:%1 /Index:1 /ApplyDir:W:\ /Compact
+if not %COMPACTOS%.==Y. dism /Apply-Image /ImageFile:%1 /Index:1 /ApplyDir:W:\
 @echo *********************************************************************
 @echo == Copy boot files to the System partition ==
 W:\Windows\System32\bcdboot W:\Windows /s S:
